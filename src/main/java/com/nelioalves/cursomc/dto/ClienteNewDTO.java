2f7eb3043dba0fcc394af9ce9ae30fb7434502b6
Renan Figueiredo <renanpdef@gmail.com>
2020-10-22 14:56:37 -0300

    Validacao customizada de CPF ou CNPJ na insercao de Cliente

--- src/main/java/com/nelioalves/cursomc/dto/ClienteNewDTO.java
+++ src/main/java/com/nelioalves/cursomc/dto/ClienteNewDTO.java
@@ -2,21 +2,45 @@ package com.nelioalves.cursomc.dto;
 
 import java.io.Serializable;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+
+import com.nelioalves.cursomc.services.validation.ClienteInsert;
+
+@ClienteInsert
 public class ClienteNewDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
+	@NotEmpty(message = "Peenchimento obrigatório!")
+	@Length(min=5, max=120, message="O tamanho deve ser entre 5 e 120 caracteres")
 	private String nome;
+	
+	@NotEmpty(message = "Peenchimento obrigatório!")
+	@Email(message = "E-mail inválido!")
 	private String email;
+	
+	@NotEmpty(message = "Peenchimento obrigatório!")
 	private String cpfOuCnpj;
+	
 	private Integer tipo;
 
+	@NotEmpty(message = "Peenchimento obrigatório!")
 	private String logradouro;
+	
+	@NotEmpty(message = "Peenchimento obrigatório!")
 	private String numero;
+	
 	private String complemento;
 	private String bairro;
+	
+	@NotEmpty(message = "Peenchimento obrigatório!")
 	private String cep;
 
+	@NotEmpty(message = "Peenchimento obrigatório!")
 	private String telefone1;
+	
 	private String telefone2;
 	private String telefone3;
 
